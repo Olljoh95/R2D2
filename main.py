6c377593744a836303916b9a42945cb822eff9f8
Olljoh95 <Ollejohansson1995@gmail.com>
2021-02-16 14:23:04 +0100

    firstCommit

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix="!")
+token = os.getenv("discordToken")
+
+@client.event
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    print("I am online")
+
+@client.command()
+async def ping(ctx) :
+    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+
+@client.command(name="whoami")
+async def whoami(ctx) :
+    await ctx.send(f"You are {ctx.message.author.name}")
+
+@client.command(name="warn")
+async def whoami(ctx) :
+    await ctx.send(f"You are warned blip blop")
+
+@client.command()
+async def clear(ctx, amount=3) :
+    await ctx.channel.purge(limit=amount)
+
+
+client.run(token)
