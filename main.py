9f51da5adcd79e85b1f5b76c460363fe7009d039
Olljoh95 <Ollejohansson1995@gmail.com>
2021-02-16 14:38:18 +0100

    Added ban

--- main.py
+++ main.py
@@ -18,9 +18,48 @@ async def ping(ctx) :
 async def whoami(ctx) :
     await ctx.send(f"You are {ctx.message.author.name}")
 
-@client.command(name="warn")
-async def whoami(ctx) :
-    await ctx.send(f"You are warned blip blop")
+with open('reports.json', encoding='utf-8') as f:
+  try:
+    report = json.load(f)
+  except ValueError:
+    report = {}
+    report['users'] = []
+
+client = discord.ext.commands.Bot(command_prefix = '?')
+
+@client.command(pass_context = True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def warn(ctx,user:discord.User,*reason:str):
+  if not reason:
+    await client.say("Please provide a reason")
+    return
+  reason = ' '.join(reason)
+  for current_user in report['users']:
+    if current_user['name'] == user.name:
+      current_user['reasons'].append(reason)
+      break
+  else:
+    report['users'].append({
+      'name':user.name,
+      'reasons': [reason,]
+    })
+  with open('reports.json','w+') as f:
+    json.dump(report,f)
+
+@client.command(pass_context = True)
+async def warnings(ctx,user:discord.User):
+  for current_user in report['users']:
+    if user.name == current_user['name']:
+      await client.say(f"{user.name} has been reported {len(current_user['reasons'])} times : {','.join(current_user['reasons'])}")
+      break
+  else:
+    await client.say(f"{user.name} has never been reported")  
+
+@warn.error
+async def kick_error(error, ctx):
+  if isinstance(error, MissingPermissions):
+      text = "Sorry {}, you do not have permissions to do that!".format(ctx.message.author)
+      await client.send_message(ctx.message.channel, text)   
 
 @client.command()
 async def clear(ctx, amount=3) :
